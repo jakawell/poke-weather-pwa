d7586ae68d232169e8beaf68f616ca5096f755c8
Jesse Kawell <jakawell@gmail.com>
2017-12-27 17:02:45 -0500

    Added NG service worker configuration and began UI development

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { MatToolbarModule, MatSidenavModule, MatCardModule } from '@angular/material';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    ),
+    environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : [],
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
